c87ecec05789790430797cba4fc19e63ba277607
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-10-13 14:10:34 -0300

    envolvendo app no contexto

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import NewsProvider from './context/NewsProvider.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NewsProvider>
+        <App />
+      </NewsProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
