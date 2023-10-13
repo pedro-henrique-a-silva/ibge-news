952d596e588913ba1c5cc92f684d840361670688
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-10-13 19:58:58 -0300

    fazendo renderWithRouter

--- src/utils/renderWithRouter.tsx
+++ src/utils/renderWithRouter.tsx
@@ -1,12 +1,21 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import NewsProvider from '../context/NewsProvider.tsx'
+
 
 export const renderWithRouter = (ui: JSX.Element, { route = '/' } = {}) => {
   window.history.pushState({}, '', route);
 
+
   return {
+    ...render(
+      <BrowserRouter>
+        <NewsProvider>
+          {ui}
+        </NewsProvider>
+      </BrowserRouter>
+    ),
     user: userEvent.setup(),
-    ...render(ui, { wrapper: BrowserRouter }),
   };
 };
