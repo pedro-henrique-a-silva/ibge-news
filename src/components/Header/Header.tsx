bfbbf18d2abf6b1c40be8b81680e1a29cab8a7a8
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-10-13 16:19:12 -0300

    Criando o header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { HeaderWrapper } from './style';
+
+function Header() {
+  return (
+    <div>
+      <HeaderWrapper>
+        <h1>IBGE news</h1>
+      </HeaderWrapper>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/Header/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+    background-color: #2288B3;
+    width: 100%;
+    height: 70px;
+
+    & h1 {
+        font-size: 1.5rem;
+        color: #fff;
+        text-align: center;
+        padding: 20px;
+    }
+`
