8e6838323d74358b1630f99213bd17cafd2d2791
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-10-14 15:50:00 -0300

    Removendo uma div

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,9 @@ import { HeaderWrapper } from './style';
 
 function Header() {
   return (
-    <div>
-      <HeaderWrapper>
-        <h1>IBGE news</h1>
-      </HeaderWrapper>
-    </div>
+    <HeaderWrapper>
+      <h1>IBGE news</h1>
+    </HeaderWrapper>
   );
 }
 
