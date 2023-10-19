b1ffe58f61ab4c4aad1ff51e1efeb03490721791
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-10-19 13:58:22 -0300

    funcionalidade do butão moreNews

--- src/components/MoreNews/MoreNews.tsx
+++ src/components/MoreNews/MoreNews.tsx
@@ -1,9 +1,31 @@
+import { useContext } from 'react';
 import { MoreNewsWrapper } from './styles';
+import NewsContext from '../../context/NewsContext';
+import { fetchAPIFiltered, fetchAPI, FILTERS_NAME } from '../../utils/utils';
 
-function MoreNews() {
+type MoreNewsType = {
+  filterHighlight: number
+};
+
+function MoreNews(props: MoreNewsType) {
+  const { filterHighlight } = props;
+  const { pagination, updatePagination, moreNews } = useContext(NewsContext);
+
+  const handleMoreNews = async () => {
+    if (filterHighlight === 2 || filterHighlight === 3) {
+      const data = await fetchAPIFiltered(FILTERS_NAME[filterHighlight], 10, pagination);
+      moreNews(data.items);
+      updatePagination('');
+      return;
+    }
+
+    const data = await fetchAPI(10, pagination);
+    moreNews(data.items);
+    updatePagination('');
+  };
   return (
     <MoreNewsWrapper>
-      <button>MoreNews</button>
+      <button onClick={ handleMoreNews }>MoreNews</button>
     </MoreNewsWrapper>
   );
 }
